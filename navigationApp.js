c0b18131c05299df928f0abaa48b977f4d4b6ea3
Ghanashyam Patil <88094853+patil-gm@users.noreply.github.com>
2023-04-22 17:48:21 +0530

    Create navigationApp.js

---
+++ navigationApp.js
@@ -0,0 +1,29 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Create from './components/Create';
+import BlogDetails from './components/BlogDetails';
+import NotFound from './components/NotFound';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <Router>
+          <div className="App">
+            <Navbar/>
+            <div className="content">
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/create" element={<Create/>} />
+                <Route path="/blogs/:id" element={<BlogDetails/>} />
+                <Route path="*" element={<NotFound/>} />
+            </Routes>
+            </div>
+        </div>
+    </Router>
+    
+  );
+}
+
+export default App;
