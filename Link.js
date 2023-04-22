40611cc708fa410551fdc4b808dd64ce67bafbc3
Ghanashyam Patil <88094853+patil-gm@users.noreply.github.com>
2023-04-22 17:51:11 +0530

    Create Link.js

---
+++ Link.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Demo=()=>{
+    return (
+        <div className="notfound">
+                <h2>use Link tutorial..!</h2>
+                <Link to='/'>Back to the home page...!</Link>
+        </div>
+    )
+}
+
+export default Demo ;
