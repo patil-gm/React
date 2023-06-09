50de90da7c38b12e09c9eb19539b80eb45a58f2e
Ghanashyam Patil <88094853+patil-gm@users.noreply.github.com>
2023-06-09 11:44:30 +0530

    Update 5_useCustomHook.js

--- 5_useCustomHook.js
+++ 5_useCustomHook.js
@@ -1,29 +1,32 @@
 // the of the custom hook should always be start with 'use' word
 // here url is passed as a prop
 
-const useFetch=(url)=>{
-    const [Data, setData] = useState(null);
-    const [isPending, setisPending] = useState(true);
-    const [error,setError]=useState(null);
+import { useEffect,useState } from "react";
+import axios from 'axios'
 
-    useEffect(()=>{
+const useFetch=(url)=>{
+  const [data, setData] = useState(null);
+  const [isPending, setisPending] = useState(true);
+  const [error,setError]=useState(null);
+
+  useEffect(()=>{
 //       This setTimeoutfuction used to simulate live loading of the page 
 //       Don't use in real world projects 
-        setTimeout(()=>{
-            axios.get(url)
-                .then(res=>{
-                    setData(res.data); 
-                    setisPending(false); 
-                    setError(null);
-                })
-                .catch((err)=>{
-                      setisPending(false);
-                      setError(err.message);
-                 }) 
-        },1000)
-      
-    },[])
-    return {isPending,error,Data}
+      setTimeout(()=>{
+          axios.get(url)
+              .then(res=>{
+                  setData(res.data); 
+                  setisPending(false); 
+                  setError(null);
+              })
+              .catch((err)=>{
+                    setisPending(false);
+                    setError(err.message);
+               }) 
+      },1000)
+    
+  },[])
+  return {isPending,error,data}
 }
 
 export default useFetch;
@@ -32,5 +35,6 @@ export default useFetch;
 // pass the url to hook and store the data data
 // their is no need to create the below variabbles as useState
 // use the data to show on dashboard
+// below line used in page where we want the data
 const {Data:workouts ,isPending,error} = useFetch('/api/workouts');
 
